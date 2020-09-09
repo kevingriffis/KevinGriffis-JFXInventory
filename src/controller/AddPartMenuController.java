0d24213b6f69b57ceed9aaf47ecafe4141b625ec
Kevin Griffis <kevingriffis@gmail.com>
2020-09-08 22:30:55 -0500

    Created skeleton controller code for AddPartMenuController

--- src/controller/AddPartMenuController.java
+++ src/controller/AddPartMenuController.java
@@ -1,4 +1,58 @@
 package controller;
 
-public class AddPartMenuController {
+import javafx.event.ActionEvent;
+import javafx.fxml.Initializable;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+import javafx.scene.control.RadioButton;
+import javafx.scene.control.TextField;
+
+
+public class AddPartMenuController implements Initializable {
+
+    @FXML
+    private RadioButton inHouseRdoBtn;
+
+    @FXML
+    private RadioButton outsourcedRdoBtn;
+
+    @FXML
+    private Label inOutLabel;
+
+    @FXML
+    private TextField nameTxt;
+
+    @FXML
+    private TextField invTxt;
+
+    @FXML
+    private TextField priceCostTxt;
+
+    @FXML
+    private TextField inOutTxt;
+
+    @FXML
+    private TextField maxTxt;
+
+    @FXML
+    private TextField minTxt;
+
+    @FXML
+    void onActionCancel(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onActionSave(ActionEvent event) {
+
+    }
+
+
+    @Override
+    public void initialize(URL url, ResourceBundle rb) {
+
+    }
 }
