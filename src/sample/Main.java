a96c97b5758c6a3d1276b1d95048f93fb9bb26da
Kevin Griffis <kevingriffis@gmail.com>
2020-09-13 19:40:13 -0500

    Added sample data for testing

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -5,6 +5,10 @@ import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.stage.Stage;
+import model.InHouse;
+import model.Inventory;
+import model.Outsourced;
+import model.Product;
 
 public class Main extends Application {
 
@@ -17,6 +21,26 @@ public class Main extends Application {
     }
 
     public static void main(String[] args){
+        // Sample data for loading into views and testing
+        // Comment out if not needed
+        InHouse part1 = new InHouse(1, "InHouse Part 1", 1.99, 20, 5, 100, 42 );
+        InHouse part2 = new InHouse(2, "InHouse Part 2", 50.00, 3, 1, 12, 30 );
+        Outsourced part3 = new Outsourced(3, "Acme Portable Hole", 99.99, 3, 1, 10, "Acme Corporation");
+
+        Product prod1 = new Product(1, "Super Widget", 290.00, 20, 10, 100);
+        Product prod2 = new Product(2, "Mini Widget", 49.95, 33, 20, 200);
+
+        prod1.addAssociatedPart(part1);
+        prod1.addAssociatedPart(part3);
+        prod2.addAssociatedPart(part1);
+        prod2.addAssociatedPart(part2);
+
+        Inventory.addPart(part1);
+        Inventory.addPart(part2);
+        Inventory.addPart(part3);
+        Inventory.addProduct(prod1);
+        Inventory.addProduct(prod2);
+
         launch(args);
     }
 }
