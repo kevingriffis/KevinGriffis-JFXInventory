7d61e3a3f5262549906764424bd910944a1d8402
Kevin Griffis <kevingriffis@gmail.com>
2020-09-13 22:52:52 -0500

    Added code to invoke the Add Parts screen for the Add button under Parts

--- src/controller/MainMenuController.java
+++ src/controller/MainMenuController.java
@@ -2,19 +2,28 @@ package controller;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
 import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.stage.Stage;
 import model.Inventory;
 import model.Part;
 import model.Product;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
 
 public class MainMenuController implements Initializable {
+
+    Stage stage;
+    Parent scene;
 
     @FXML
     private TableView<Part> partsTableView;
@@ -58,8 +67,12 @@ public class MainMenuController implements Initializable {
     }
 
     @FXML
-    void onActionPartsAdd(ActionEvent event) {
+    void onActionPartsAdd(ActionEvent event) throws IOException {
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/AddPart.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
