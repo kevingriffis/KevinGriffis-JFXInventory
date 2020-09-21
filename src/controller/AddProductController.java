b536f356ec870e83c541e1de6f022ba4de1b7ebc
Kevin Griffis <kevingriffis@gmail.com>
2020-09-20 21:19:07 -0500

    Wrote rest of code for AddProductController

--- src/controller/AddProductController.java
+++ src/controller/AddProductController.java
@@ -3,19 +3,26 @@ package controller;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 
+import java.io.IOException;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Collections;
 import java.util.ResourceBundle;
 import javafx.fxml.FXML;
-import javafx.scene.control.TableColumn;
-import javafx.scene.control.TableView;
-import javafx.scene.control.TextField;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
-import model.Inventory;
-import model.Part;
+import javafx.stage.Stage;
+import model.*;
 
 public class AddProductController implements Initializable {
+
+    Stage stage;
+    Parent scene;
 
     private ObservableList<Part> addedParts = FXCollections.observableArrayList();
 
@@ -67,6 +74,24 @@ public class AddProductController implements Initializable {
     @FXML
     private TextField partsSearchField;
 
+    private int findNextAvailableProductId () {
+        // Same algorithm as findNextAvailablePartId, but applied to products
+        // This algorithm will not reuse deleted IDs that are between
+        // existing IDs
+        ArrayList<Integer> idArrayList = new ArrayList<Integer>();
+        int greatest;
+
+        for (Product product :
+                Inventory.getAllProducts()){
+            idArrayList.add(product.getId());
+        }
+
+        Collections.sort(idArrayList);
+
+        greatest = idArrayList.get(idArrayList.size() - 1);
+        return ++greatest;
+    }
+
     private void displayAddPartsInTableView(ObservableList<Part> partList) {
         addPartsTableView.setItems(partList);
 
@@ -87,17 +112,22 @@ public class AddProductController implements Initializable {
 
     @FXML
     void onActionAdd(ActionEvent event) {
+        addedParts.add(addPartsTableView.getSelectionModel().getSelectedItem());
 
     }
 
     @FXML
-    void onActionCancel(ActionEvent event) {
+    void onActionCancel(ActionEvent event) throws IOException {
 
+        stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+        scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+        stage.setScene(new Scene(scene));
+        stage.show();
     }
 
     @FXML
     void onActionDelete(ActionEvent event) {
-
+        addedParts.remove(deletePartsTableView.getSelectionModel().getSelectedItem());
     }
 
     @FXML
@@ -117,8 +147,37 @@ public class AddProductController implements Initializable {
     }
 
     @FXML
-    void onActionSave(ActionEvent event) {
+    void onActionSave(ActionEvent event) throws IOException {
 
+        try {
+            int id = findNextAvailableProductId();
+            String name = nameTxt.getText();
+            double price = Double.parseDouble(priceTxt.getText());
+            int stock = Integer.parseInt(invTxt.getText());
+            int min = Integer.parseInt(minTxt.getText());
+            int max = Integer.parseInt(maxTxt.getText());
+
+            Product product = new Product(id, name, price, stock, min, max);
+
+            for (Part part : addedParts) {
+                product.addAssociatedPart(part);
+            }
+
+            Inventory.addProduct(product);
+
+            // Return to Main Menu
+            stage = (Stage)((Button)event.getSource()).getScene().getWindow();
+            scene = FXMLLoader.load(getClass().getResource("/view/MainMenu.fxml"));
+            stage.setScene(new Scene(scene));
+            stage.show();
+
+        }
+        catch (NumberFormatException e) {
+            Alert alert = new Alert(Alert.AlertType.WARNING);
+            alert.setTitle("Warning");
+            alert.setContentText("Please enter valid values for each field!");
+            alert.showAndWait();
+        }
     }
 
     @Override
